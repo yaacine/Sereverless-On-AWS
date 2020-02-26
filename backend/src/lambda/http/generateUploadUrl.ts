a66c0f3a1eca01676ccebb209cf78a560e91bd86
yaacine <gy_zidelmal@esi.dz>
2020-02-26 10:28:31 +0100

    getTodos getting all records

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -24,8 +24,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       uploadUrl: url
     })
   }
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
 }
 
 
